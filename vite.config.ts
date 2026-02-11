6492f8cbbb7f2ec8611410d1403a315d64aa2351
Jonathan Colton <29485573+jsc17@users.noreply.github.com>
2026-02-11 09:38:06 -0500

    use keyfiles for dev ssl

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 import { sveltePhosphorOptimize } from "phosphor-svelte/vite";
+// @ts-ignore
+import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
+import fs from "fs";
 
 export default defineConfig({
-	plugins: [sveltekit(), basicSsl(), sveltePhosphorOptimize()],
+	plugins: [crossOriginIsolation(), sveltekit(), sveltePhosphorOptimize()],
 	server: {
-		https: {}, // same as "--https" flag
+		headers: {
+			"Cross-Origin-Opener-Policy": "same-origin",
+			"Cross-Origin-Embedder-Policy": "require-corp"
+		},
+		https: {
+			key: fs.readFileSync("localhost-key.pem"),
+			cert: fs.readFileSync("localhost.pem")
+		}, // same as "--https" flag
 		host: true, // same as "--host" flag
 		fs: {
 			strict: false
@@ -18,6 +27,6 @@ export default defineConfig({
 		}
 	},
 	optimizeDeps: {
-		exclude: ["chromium-bidi"]
+		exclude: ["chromium-bidi", "@sqlite.org/sqlite-wasm"]
 	}
 });

1a4dba1bce817192dcb0172827461f821cfcbb8e
Jonathan Colton <29485573+jsc17@users.noreply.github.com>
2026-03-20 10:06:19 -0400

    remove unused popover

--- src/lib/unitsearch/index.js
+++ src/lib/unitsearch/index.js
@@ -1,7 +1,6 @@
 import SearchFilters from "./SearchFilters.svelte";
 import SearchParameters from "./SearchParameters.svelte";
 import SearchResults from "./SearchResults.svelte";
-import EraFactionInfoPopover from "./EraFactionInfoPopover.svelte";
 import DamageSortPopover from "./DamageSortPopover.svelte";
 
-export { SearchFilters, SearchParameters, SearchResults, DamageSortPopover, EraFactionInfoPopover };
+export { SearchFilters, SearchParameters, SearchResults, DamageSortPopover };

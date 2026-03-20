01c228719c6076f3ff045ef4a30bc67f6d0c258f
Jonathan Colton <29485573+jsc17@users.noreply.github.com>
2026-03-20 10:31:31 -0400

    fix formation info icon

--- src/lib/icons/index.ts
+++ src/lib/icons/index.ts
@@ -4,5 +4,6 @@ import SortAscendingIcon from "$lib/icons/sort-ascending.svg?component";
 import SortDescendingIcon from "$lib/icons/sort-descending.svg?component";
 import DragIndicatorIcon from "$lib/icons/drag-indicator.svg?component";
 import TrashIcon from "$lib/icons/trash.svg?component";
+import InformationIcon from "$lib/icons/information.svg?component";
 
-export { GearIcon, SortIcon, SortAscendingIcon, SortDescendingIcon, DragIndicatorIcon, TrashIcon };
+export { GearIcon, SortIcon, SortAscendingIcon, SortDescendingIcon, DragIndicatorIcon, TrashIcon, InformationIcon };
--- src/routes/listbuilder/components/ListbuilderComponents/FormationInfoPopover.svelte
+++ src/routes/listbuilder/components/ListbuilderComponents/FormationInfoPopover.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { Popover, Separator } from "$lib/generic";
+	import { InformationIcon } from "$lib/icons";
 
 	type Props = {
 		formationStats: any;
@@ -12,7 +13,7 @@
 	{#snippet trigger()}
 		<div class="formation-info-trigger">
 			<p class="formation-info-trigger-text">PV: {formationStats.totalPV}</p>
-			<img class="info-button-icon" src="/icons/information.svg" alt="information" />
+			<InformationIcon width="15" height="15" fill="var(--surface-color-light-text-color)" />
 		</div>
 	{/snippet}
 
@@ -65,7 +66,8 @@
 	.formation-info-trigger {
 		padding: 2px 4px;
 		display: flex;
-		gap: 2px;
+		gap: 4px;
+		align-items: center;
 	}
 	.formation-info-trigger-text {
 		color: var(--surface-color-light-text-color);
